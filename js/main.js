c5f53e8e71dc11c7f4addd3aabbc9b461a7ab983
Alexander Lapshin <nihsxela@gmail.com>
2023-06-22 11:11:03 +0300

    изменяет наименование массивов

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //Oписание фотографии.
-const specification = [
+const descriptions = [
   'Лицо человека, вышедшего из матрицы',
   'В этот день родили меня на свет...',
   'Если не поставишь лайк, я тебя найду!',
@@ -12,7 +12,7 @@ const specification = [
 ];
 
 //Сообщения от комментатора
-const message = [
+const messages = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -32,39 +32,34 @@ const names = [
   'Вашингтон',
 ];
 
-//Возвращает рандомное число
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
+//Функция возвращает рандомное число в необходимом диапазоне
+const getRandomNumberInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 //Функция возвращает количество лайков
 const minLikesAmount = 15;
 const maxLikesAmount = 200;
-const getLikes = () => Math.floor(Math.random() * (maxLikesAmount - minLikesAmount + 1)) + minLikesAmount;
+const getLikes = () => getRandomNumberInRange(minLikesAmount, maxLikesAmount);
 
 const minAvatarNumber = 1;
 const maxAvatarNumber = 6;
 
 //Функция возвращает номер аватара
-const getAvatarNumber = () => Math.floor(Math.random() * (maxAvatarNumber - minAvatarNumber + 1)) + minAvatarNumber;
+const getAvatarNumber = () => getRandomNumberInRange(minAvatarNumber, maxAvatarNumber);
 
 //создает массив из списка комментаторов
-const createCommentUser = new Array(30).fill(1).map((currentValue, index) => ({
+const comments = new Array(30).fill(1).map((currentValue, index) => ({
   id: index + 1,
   avatar: `img/avatar-${getAvatarNumber()}.svg`,
-  message: message[getRandomInteger(0, message.length - 1)],
-  name: names[getRandomInteger(0, names.length - 1)]
+  message: messages[getRandomNumberInRange(0, messages.length - 1)],
+  name: names[getRandomNumberInRange(0, names.length - 1)]
 }));
 
 //создает массив описаний фотографий
-const createPost = new Array(25).fill(1).map((currentValue, index) => ({
+const posts = new Array(25).fill(1).map((currentValue, index) => ({
   id: index + 1, //Идентификатор фотографии
   url: `photos/${index + 1}.jpg`, //Адрес картинки
-  description: specification[getRandomInteger(0, specification.length - 1)], //Описание фотографии.
+  description: descriptions[getRandomNumberInRange(0, descriptions.length - 1)], //Описание фотографии.
   likes: getLikes(), //Количество лайков, поставленных фотографии.
-  comments: createCommentUser[getRandomInteger(0, message.length - 1)]
+  comments: comments[getRandomNumberInRange(0, messages.length - 1)]
 }));
-createPost();
+posts();
